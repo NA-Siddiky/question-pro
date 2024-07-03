11e8dd91277fc82f975954a6ccf821ec063b661f
NA-Siddiky <siddiky.academic@gmail.com>
2024-07-03 19:03:54 +0600

    done button function

--- src/components/Common/Button/Button.tsx
+++ src/components/Common/Button/Button.tsx
@@ -1,20 +1,30 @@
 // ButtonComponent.tsx
 import React from "react";
+import "./Button.scss";
 
 interface ButtonComponentProps {
-  onClick?: () => void;
+  onClick: () => void;
   children?: string;
+  isClicked?: boolean; // Prop to indicate if the button has been clicked
 }
 
-// ButtonComponent.tsx
 const ButtonComponent: React.FC<ButtonComponentProps> = ({
   children,
   ...props
 }) => {
+  // Handle click events
+  const handleClick = () => {
+    props.onClick(); // Call the passed onClick function
+  };
+
+  // Use the isClicked prop to conditionally apply a class
+  const buttonClass = props.isClicked ? "clicked" : ""; // Apply 'clicked' class if isClicked is true
+
   return (
     <button
       {...props}
-      style={{ padding: "10px", margin: "5px", cursor: "pointer" }}
+      className={`button-style ${buttonClass}`} // Combine base class with conditional class
+      onClick={handleClick}
     >
       {children}
     </button>
--- src/components/MyComponent/MyComponent.tsx
+++ src/components/MyComponent/MyComponent.tsx
@@ -1,24 +1,34 @@
-// file: src/components/MyComponent/MyComponent.tsx
-
+// MyComponent.tsx
 import React, { useState } from "react";
 import ButtonComponent from "../Common/Button/Button";
 
 const MyComponent: React.FC = () => {
+  const [activeButton, setActiveButton] = useState<string | null>(null);
   const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(count + 1);
+  const handleClick = (buttonId: string) => {
+    setActiveButton(buttonId);
+    if (buttonId === "countable") {
+      setCount(count + 1);
+    }
   };
 
   return (
     <>
-      <h2>Welcome to My Component Page </h2>
       <div>
-        <h1>Simple Click Counter {count}</h1>
-        <ButtonComponent onClick={handleClick}>
+        <p>Only one button can be active at {count} time.</p>
+        {/* Pass the button ID and check if it matches the activeButton state */}
+        <ButtonComponent
+          onClick={() => handleClick("countable")}
+          isClicked={activeButton === "countable"}
+        >
           Countable Button
         </ButtonComponent>
-        <ButtonComponent>Dump Button</ButtonComponent>
+        <ButtonComponent
+          onClick={() => handleClick("dump")}
+          isClicked={activeButton === "dump"}
+        >
+          Dump Button
+        </ButtonComponent>
       </div>
     </>
   );
